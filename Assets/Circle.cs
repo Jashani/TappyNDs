baf5c84e32deafb5985ce344c43598de73a9a8e5
Jashani <jashaniyt@gmail.com>
2017-11-16 14:54:48 +0200

    Circles now spawn with different colours and cap at 10. Press/tap to destroy.

---
+++ Assets/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<SpriteRenderer> ().color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D () {
+		gameObject.transform.position = Camera.main.ScreenToWorldPoint (new Vector3 (Random.Range (0, Screen.width), Random.Range (0, Screen.height), Camera.main.farClipPlane / 2));
+		Debug.Log ("Collision.");
+	}
+
+	void OnMouseDown () {
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/CircleSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleSpawner : MonoBehaviour {
+
+	public GameObject circle;
+
+	private int maxChildren = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isTimeToSpawn () && gameObject.transform.childCount < maxChildren) {
+			Spawn ();
+		}
+	}
+
+	bool isTimeToSpawn() {
+		float spawnDelay = 10;
+		//float spawnsPerSecond = spawnDelay;
+		//Debug.Log (spawnsPerSecond);
+
+		if (Time.deltaTime > spawnDelay) 
+			Debug.LogWarning ("Spawn rate capped by frame rate.");
+
+		float threshold = spawnDelay * Time.deltaTime / 10f ;
+
+		return (Random.value < threshold);
+	}
+
+	void Spawn() {
+		Vector3 screenPosition = Camera.main.ScreenToWorldPoint (new Vector3 (Random.Range (0, Screen.width), Random.Range (0, Screen.height), Camera.main.farClipPlane / 2));
+		GameObject newCircle = Instantiate (circle, screenPosition, Quaternion.identity);
+		newCircle.transform.parent = gameObject.transform;
+	}
+}
